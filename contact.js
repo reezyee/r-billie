321cd8813eebda7a178c87724d45b3f0ff406c78
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2023-08-20 17:29:12 +0700

    test

--- contact.js
+++ contact.js
@@ -1,12 +1,10 @@
-<script type="text/javascript">
-//<![CDATA[
-if (window.jstiming) window.jstiming.load.tick('widgetJsBefore');
+<><script type="text/javascript">
+//< />![CDATA[
+    if (window.jstiming) window.jstiming.load.tick('widgetJsBefore');
 //]]>
-</script>
-<script src="https://www.blogger.com/static/v1/widgets/2271878333-widgets.js" type="text/javascript"></script>
-<script type="text/javascript">
-//<![CDATA[
-if (typeof(BLOG_attachCsiOnload) != 'undefined' && BLOG_attachCsiOnload != null) { window['blogger_templates_experiment_id'] = "templatesV1";window['blogger_blog_id'] = '3263220808940184577';BLOG_attachCsiOnload(''); }_WidgetManager._Init('//www.blogger.com/rearrange?blogID\x3d3263220808940184577','//infiniteidn.blogspot.com/','3263220808940184577');
-_WidgetManager._RegisterWidget('_ContactFormView', new _WidgetInfo('ContactForm1', 'footer1', null, document.getElementById('ContactForm1'), {'contactFormMessageSendingMsg': 'rezasapitra514@gmail.com', 'contactFormMessageSentMsg': 'Your message has been sent.', 'contactFormMessageNotSentMsg': 'Message could not be sent. Please try again later.', 'contactFormInvalidEmailMsg': 'A valid email address is required.', 'contactFormEmptyMessageMsg': 'Message field cannot be empty.', 'title': 'Contact Form', 'blogId': '3263220808940184577', 'contactFormNameMsg': 'Name', 'contactFormEmailMsg': 'Email', 'contactFormMessageMsg': 'Message', 'contactFormSendMsg': 'Send', 'submitUrl': 'rezasapitra@gmail.com'}, 'displayModeFull'));
-//]]>
-</script>
+</script><script src="https://www.blogger.com/static/v1/widgets/2271878333-widgets.js" type="text/javascript"></script><script type="text/javascript">
+//< />![CDATA[
+        if (typeof(BLOG_attachCsiOnload) != 'undefined' && BLOG_attachCsiOnload != null) {window['blogger_templates_experiment_id'] = "templatesV1"};window['blogger_blog_id'] = '3263220808940184577';BLOG_attachCsiOnload(''); _WidgetManager._Init('//www.blogger.com/rearrange?blogID\x3d3263220808940184577','//infiniteidn.blogspot.com/','3263220808940184577');
+        _WidgetManager._RegisterWidget('_ContactFormView', new _WidgetInfo('ContactForm1', 'footer1', null, document.getElementById('ContactForm1'), {'contactFormMessageSendingMsg'}: 'rezasapitra514@gmail.com', 'contactFormMessageSentMsg': 'Your message has been sent.', 'contactFormMessageNotSentMsg': 'Message could not be sent. Please try again later.', 'contactFormInvalidEmailMsg': 'A valid email address is required.', 'contactFormEmptyMessageMsg': 'Message field cannot be empty.', 'title': 'Contact Form', 'blogId': '3263220808940184577', 'contactFormNameMsg': 'Name', 'contactFormEmailMsg': 'Email', 'contactFormMessageMsg': 'Message', 'contactFormSendMsg': 'Send', 'submitUrl': 'rezasapitra@gmail.com'}, 'displayModeFull'));
+    //]]{">"}
+    </script></>
